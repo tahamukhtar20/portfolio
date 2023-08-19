7670aeea23d81c2a51d9be6dbb659c9790731d5b
Muhammad Taha Mukhtar <91777330+tahamukhtar20@users.noreply.github.com>
2023-08-19 22:08:12 +0500

    email bug fix v1

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,37 +1,49 @@
 import nodemailer from "nodemailer";
-import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const req = await request.json();
   try {
+    const req = await request.json();
     const transporter = nodemailer.createTransport({
       port: 465,
       host: "smtp.gmail.com",
       auth: {
-        user: process.env.NEXT_PUBLIC_EMAIL,
-        pass: process.env.NEXT_PUBLIC_PASSWORD,
+        user: process.env.NEXT_PUBLIC_EMAIL!,
+        pass: process.env.NEXT_PUBLIC_PASSWORD!,
       },
     });
+
     const mailData = {
-      from: process.env.NEXT_PUBLIC_EMAIL,
-      to: process.env.NEXT_PUBLIC_EMAIL,
+      from: process.env.NEXT_PUBLIC_EMAIL!,
+      to: process.env.NEXT_PUBLIC_EMAIL!,
       subject: `Message from ${req.name} [${req.email}] - Portfolio`,
-      text: req.message + " | Sent from: " + req.email,
-      html: `<div>${req.message}</div><p>Sent from:
-        ${req.email}</p>`,
+      text: `${req.message} | Sent from: ${req.email}`,
+      html: `
+        <div>${req.message}</div>
+        <p>Sent from: ${req.email}</p>
+      `,
     };
-    transporter.sendMail(mailData, function (err: any, info: any) {
-      if (err) throw new Error(err);
-    });
+    try {
+      const info = await transporter.sendMail(mailData);
+      console.log("Email sent:", info.response);
+      return new Response(JSON.stringify({ message: "Email sent" }), {
+        headers: { "content-type": "application/json" },
+        status: 200,
+      });
+    } catch (err) {
+      console.error("Error sending email:", err);
+      return new Response(
+        JSON.stringify({ error: `An error occurred ${err}` }),
+        {
+          headers: { "content-type": "application/json" },
+          status: 500,
+        }
+      );
+    }
   } catch (error) {
     console.error("Error:", error);
-    return new NextResponse(JSON.stringify({ error: "An error occurred" }), {
+    return new Response(JSON.stringify({ error: "An error occurred" }), {
       headers: { "content-type": "application/json" },
       status: 500,
     });
   }
-  return new NextResponse(JSON.stringify({ message: "Email sent" }), {
-    headers: { "content-type": "application/json" },
-    status: 200,
-  });
 }
